c65a2601dccbf8fc8ad4d2ee6686bce435106b97
defranjo <joe.defranco@sheridancollege.ca>
2024-05-22 12:10:30 -0400

    Fifth comment

---
+++ src/studentlist3/PartTimeStudent.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package studentlist3;
+
+/**
+ *
+ * @author joede
+ */
+public class PartTimeStudent {
+    
+    // TODO
+    
+}
